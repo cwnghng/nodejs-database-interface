8a80c9b4a6c7e9471a3a482f9df6f5a3fbddd217
cwnghng <aurum@chuas-air.lan>
2022-12-08 07:26:12 +0800

    Switched to using Sequelize

--- src/DbInterface.js
+++ src/DbInterface.js
@@ -1,21 +1,102 @@
 import mysql from 'mysql2';
+import { Sequelize } from 'sequelize';
 import Commands from './DbInterface.Commands.js';
+import Logger from './Logger';
 
-const {
+async function DbInterface ( host, username, password, database, params = {} ) {
 
-  SQL_DATABASE,
-  SQL_PASSWORD,
-  SQL_HOST
+  const db = new Sequelize( database, username, password, {
 
-} = process.env;
+    host, dialect: 'mysql', logging: message => Logger.info( message )
 
-async function DbInterface () {
+  } );
 
-  const DB = initDB();
+  try {
+
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+
+  } catch (error) {
+
+    console.error('Unable to connect to the database:', error);
+
+  }
+
+  const commands = setupCommands();
+
+  return {
+
+
+
+  }
 
   function initDB() {
 
+
     
+  }
+
+  async function isDbExist() {
+
+    const connection = mysql.createConnection( {
+
+      host, username, password
+
+    } );
+
+    connection.connect();
+
+    const sql = `SELECT schema_name FROM information_schema.schemata WHERE schema_name = '${ SQL_DATABASE }`;
+
+    return new Promise( ( resolve, reject ) => {
+
+      connection.query( sql, ( error, result ) => {
+
+        connection.end();
+
+        if ( error ) {
+
+          Logger.error( error );
+
+          reject( error );
+
+        } else if ( result.length === 0 ) {
+
+          Logger.debug( `Database ${ database } does not exist` );
+
+          resolve( false );
+
+        } else {
+
+          Logger.debug( `Database ${ database } exists` );
+
+          resolve( true );
+
+        }
+
+      } )
+
+    } )
+
+  }
+
+  async function runCommand ( cmd, params ) {
+
+    try {
+
+      const response = await cmd( DB, params );
+
+      Logger.info( response.message );
+
+      return response.result;
+
+    }
+
+    catch ( error ) {
+
+      Logger.error( error );
+
+    }
     
   }
 
